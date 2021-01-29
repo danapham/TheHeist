321d5b68c2171d2f11f3d4601b5829784f20f20f
Dana Pham <danapham12@gmail.com>
2021-01-28 20:20:21 -0600

    started on project

---
+++ TheHeist/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TheHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teamMember1 = new TeamMember();
+            teamMember1.CreateTeamMember();
+
+            //    var teamMember2 = new TeamMember();
+            //    teamMember2.CreateTeamMember();
+
+            //    var teamMember3 = new TeamMember();
+            //    teamMember3.CreateTeamMember();
+
+            //    teamMember3.Display();
+            //    teamMember2.Display();
+            teamMember1.Display();
+        }
+    }
+}
---
+++ TheHeist/TeamMember.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheHeist
+{
+    class TeamMember
+    {
+        public string Name { get; set; }
+        public double SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+        public static int Count = 0;
+
+        public TeamMember()
+        {
+            Count++;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Team Member: {Name}");
+            Console.WriteLine($"Skill Level: {SkillLevel}");
+            Console.WriteLine($"Courage Factor: {CourageFactor}");
+        }
+
+        public void CreateTeamMember()
+        {
+            Console.WriteLine("Plan Your Heist!");
+            Console.Write("Enter a team member's name: ");
+            Name = Console.ReadLine();
+
+            while (Name != "")
+            {
+                Console.WriteLine("Enter this team member's skill level: ");
+                SkillLevel = Math.Round(Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine("Enter this team member's courage factor: ");
+                CourageFactor = Convert.ToInt32(Console.ReadLine());
+
+                if (CourageFactor < 0)
+                {
+                    CourageFactor = 0;
+                }
+                else if (CourageFactor > 2)
+                {
+                    CourageFactor = 2.0;
+                }
+                CreateTeamMember();
+            }
+        }
+    }
+}
